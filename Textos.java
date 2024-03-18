2e6cf1297cc410bef76bd77ff220d02f45c7d3b2
joathan99 <46822745+joathan99@users.noreply.github.com>
2024-03-18 11:33:34 +0100

    Arreglo en la clase Textos

--- Textos.java
+++ Textos.java
@@ -1,9 +1,12 @@
+/**
+ * Clase que contiene los textos que se muestran en la aplicación.
+ */
 public class Textos {
     public static final String LISTA_EMPLEADOS = "Lista de Empleados:";
     public static final String INTRODUZCA_PORCENTAJE = "Introduzca el porcentaje de aumento de salario: ";
     public static final String ALTA_EMPLEADO = "Dando de alta a un empleado...";
     public static final String CARGO_PROGRAMADOR = "Programador";
-    public static final String CARGO_DESARROLLADOR = "Programador";
-    public static final String CARGO_DISEÑADOR = "Programador";
-    public static final String CARGO_GERENTE = "Programador";
+    public static final String CARGO_DESARROLLADOR = "Desarrolador";
+    public static final String CARGO_DISEÑADOR = "Diseñador";
+    public static final String CARGO_GERENTE = "Gerente";
 }
